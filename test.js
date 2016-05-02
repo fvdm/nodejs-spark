a3784ae67dd7d69d4918b277182c73bfa75b85a2
Franklin van de Meent <fr@nkl.in>
2016-05-02 19:38:56 +0200

    Test: added more interface checks

--- test.js
+++ test.js
@@ -10,11 +10,24 @@ var config = {
 
 var spark = app && app (config);
 
+var deviceId = process.env.testDeviceId || null;
+var device = spark && spark.device (deviceId);
 
 dotest.add ('Module', function (test) {
   test ()
     .isFunction ('fail', 'exports', app)
     .isObject ('fail', 'interface', spark)
+    .isFunction ('fail', '.devices', spark && spark.devices)
+    .isFunction ('fail', '.device', spark && spark.device)
+    .isFunction ('fail', '.device.info', device && device.info)
+    .isFunction ('fail', '.device.variable', device && device.variable)
+    .isFunction ('fail', '.device.func', device && device.func)
+    .isFunction ('fail', '.device.events', device && device.events)
+    .isFunction ('fail', '.claimDevice', spark && spark.claimDevice)
+    .isFunction ('fail', '.events', spark && spark.events)
+    .isFunction ('fail', '.publishEvent', spark && spark.publishEvent)
+    .isObject ('fail', '.accessToken', spark && spark.accessToken)
+    .isFunction ('fail', '.accessToken.list', spark && spark.accessToken && spark.accessToken.list)
     .done ();
 });
 
